2d0013388857c335a199560cb81fa1b264476a49
patagoniaguest <patagoniaguest@gmail.com>
2016-11-29 16:34:07 -0300

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -1,9 +1,24 @@
 window.sheets = (function () {
   var sheets = {};
   
+  sheets.authorize = authorize;
+  sheets.addBookingRow = addBookingRow;
+  
   return sheets;
   
   /////////////////////////////////////////////
+
+  /* Authorize the sheets API */
+  function authorize() {
+    return gapi.auth.authorize(
+      { client_id: config.apiClientId, scope: config.SCOPES, immediate: false }
+    ).then(loadSheetsApi);
+  }
+
+  /* Load the sheets client library. Returns a promise */
+  function loadSheetsApi() {
+    return gapi.client.load(config.SHEETS_API_DISCOVERY_URL);
+  }
   
   /* Add a booking row with the data passed */
   function addBookingRow(data) {
