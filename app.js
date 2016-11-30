0a4f974b71646b42d472005ed11baeb8189474a8
patagoniaguest <patagoniaguest@gmail.com>
2016-11-30 15:01:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 (function() {
-  angular.module("app", [])
+  angular.module("app", ["ngMaterial", "ngMessages"])
     .constant("SCOPES", ["https://www.googleapis.com/auth/spreadsheets"])
     .constant("SHEETS_API_DISCOVERY_URL", 'https://sheets.googleapis.com/$discovery/rest?version=v4')
     .run(function ($rootScope) {
