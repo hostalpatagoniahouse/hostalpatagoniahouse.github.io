053b7886fd553d43e6f0ededc1510fe94f37c87d
patagoniaguest <patagoniaguest@gmail.com>
2016-11-30 14:24:20 -0300

    Update entry-controller.js

--- entry-controller.js
+++ entry-controller.js
@@ -1,40 +1,19 @@
 'use strict';
 
-window.entryController = (function () {
-  var entryController = {};
-  
-  init();
-  
-  return entryController;
-  
-  /////////////////////////////////////////////
-  
-  function init() {
-    var addButton = document.getElementById('add-button');
+(function () {
+  angular.module("app")
+    .controller("EntryController", function ($scope, $rootScope, utils, sheetsService) {
+      $scope.entry = {};
     
-    addButton.addEventListener('click', function (event) {
-      event.preventDefault();
-      
-      var bookingRow = getBookingRow();
-      
-      if (validateBookingRow(bookingRow)) {
-        sheets.addBookingRow(bookingRow);
+      $scope.addEntry = function () {
+        sheetsService.addBookingRow(getFormattedEntry());
+      };
+    
+      function getFormattedEntry() {
+        var formattedEntry = angular.copy($scope.entry);
+        formattedEntry.date = utils.toSheetsDate(Date.now());
+        
+        return formattedEntry;
       }
     });
-  }
-  
-  function getBookingRow() {
-    var data = {};
-    
-    data.name = document.getElementById('name-input').value;
-    data.date = Date.now();
-    data.days = document.getElementById('days-input').value;
-    data.bed = document.getElementById('bed-input').value;
-    
-    return data;
-  }
-  
-  function validateBookingRow(data) {
-    return (data.name && data.date && data.days && data.bed);
-  }
 })();
