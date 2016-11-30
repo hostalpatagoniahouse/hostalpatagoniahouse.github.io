6017c7735849412ad71b666c9c97e540c3ccef49
patagoniaguest <patagoniaguest@gmail.com>
2016-11-30 18:53:24 -0300

    Create gapi-service.js

---
+++ gapi-service.js
@@ -0,0 +1,51 @@
+"use strict";
+
+(function () {
+  angular.module("app")
+    .factory("gapiService", function ($rootScope, $q, SCOPES, SHEETS_API_DISCOVERY_URL) {
+      var gapiService = {};
+      
+      gapiService.authorize = authorize;
+      
+      var gapiValueBase = gapi.client.sheets.spreadsheets.values
+      var gapiValueFunctions = ["append", "get"];
+      
+      gapiValueFunctions.forEach(function (fnName) {
+        gapiService[fnName] = function () {
+          var deferred = $q.defer();
+          
+          gapiValueBase[fnName].apply(gapiValueBase, arguments).then(function (response) {
+            deferred.resolve(response)
+          }, function (response) {
+            deferred.reject (response);
+          });
+          
+          return deferred.promise;
+        }
+      });
+      
+      return gapiService;
+      
+      ////////////////////////////////
+      
+      /* Authorize the sheets API */
+      function authorize() {
+        var deferred = $q.defer();
+        
+        gapi.auth.authorize(
+          { client_id: $rootScope.config.apiClientId, scope: SCOPES, immediate: false }
+        ).then(loadSheetsApi).then(function (response) {
+          deferred.resolve(response);
+        }, function (response) {
+          deferred.reject(response);
+        });
+        
+        return deferred.promise;
+      }
+
+      /* Load the sheets client library. Returns a promise */
+      function loadSheetsApi() {
+        return gapi.client.load(SHEETS_API_DISCOVERY_URL);
+      }
+    });
+})();
