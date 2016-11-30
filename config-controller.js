a30c507ba8c3fbdcb154ebbdddb95166749b33c2
patagoniaguest <patagoniaguest@gmail.com>
2016-11-30 14:04:42 -0300

    Update config-controller.js

--- config-controller.js
+++ config-controller.js
@@ -1,52 +1,26 @@
 'use strict';
 
-window.configController = (function () {
-  var configController = {};
-  
-  var configInputs = {
-    apiClientId: 'api-client-id-input',
-    bookingSheetId: 'booking-sheet-id-input',
-    bookingSheet: 'booking-sheet-input'
-  };
-  
-  init();
-  
-  return configController;
-  
-  ////////////////////////////////////////
-  
-  function init() {
-    preloadConfigFields();
+(function () {
+  angular.module("app")
+    .controller("ConfigController", function ($scope, $rootScope, sheetsService) {
+      var configInputs = ["apiClientId", "bookingSheetId", "bookingSheet"];
     
-    // Handle the authorize button click
-    var saveConfigButton = document.getElementById('save-config-button');
-    
-    saveConfigButton.addEventListener('click', function (event) {
-      event.preventDefault();
-      saveConfig();
-      sheets.authorize().then(function (a) {
-        mainController.showEntrySection();
+      $rootScope.config = {};
+      
+      configInputs.forEach(function (field) {
+        if (localStorage[field]) {
+          $rootScope.config[field] = localStorage[field]
+        }
       });
-    });
-  }
-  
-  /* Load the config fields from localstorage if they have been saved there */
-  function preloadConfigFields() {
-    for(var index in configInputs) { 
-      if (configInputs.hasOwnProperty(index) && localStorage[index]) {
-        document.getElementById(configInputs[index]).value = localStorage[index];
-      }
-    }
-  }
-  
-  /* Save the config fields to localstorage and refresh the config object */
-  function saveConfig() {
-    for(var index in configInputs) { 
-      if (configInputs.hasOwnProperty(index)) {
-        localStorage[index] = document.getElementById(configInputs[index]).value;
-      }
-    }
     
-    config.refreshConfig();
-  }
+      $scope.saveConfig = function () {
+        configInputs.forEach(function (field) {
+          localStorage[field] = $rootScope.config[field];
+        });
+        
+        sheetsService.authorize().then(function () {
+          $rootScope.configComplete = true;
+        });
+      }
+    });
 })();
