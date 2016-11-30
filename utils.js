dc3ce8bad59f79c95c79bc32d4f13d85ae30a264
patagoniaguest <patagoniaguest@gmail.com>
2016-11-30 15:59:24 -0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -9,6 +9,8 @@
       utils.fromSheetsDate = fromSheetsDate;
       utils.error = error;
       utils.getAlphabet = getAlphabet;
+      utils.getMonthName = getMonthName;
+      utils.getShortYear = getShortYear;
 
       var epoch = new Date(1899,11,30);
       var msPerDay = 8.64e7;
@@ -35,6 +37,17 @@
       /* Return the nth alphabet (uppercase). For n = 1 returns A */
       function getAlphabet(n) {
         return String.formCharCode(64 + n);
+      }
+    
+      /* Get spanish month name from date */
+      function getMonthName(date) {
+        var monthNames = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+        return monthNames[date.getMonth()];
+      }
+      
+      /* Get last two digits of the year from date */
+      function getShortYear(date) {
+        return parseInt(date.getFullYear().toString().substr(2,2), 10);
       }
     });
 })();
