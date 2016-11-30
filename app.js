d46d21e13a468c18f5d0b1b5f0cc6f862ff18321
patagoniaguest <patagoniaguest@gmail.com>
2016-11-30 14:07:43 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 (function() {
   angular.module("app", [])
+    .constant("SCOPES", ["https://www.googleapis.com/auth/spreadsheets"])
+    .constant("SHEETS_API_DISCOVERY_URL", 'https://sheets.googleapis.com/$discovery/rest?version=v4')
     .run(function ($rootScope) {
       $rootScope.configComplete = false;
     });
