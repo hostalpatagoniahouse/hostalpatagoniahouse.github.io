61a7d942c6e3316ba4109f0aa1ed3f46381b1eb7
patagoniaguest <patagoniaguest@gmail.com>
2016-11-30 14:12:05 -0300

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -1,51 +1,54 @@
 'use strict';
 
-window.sheets = (function () {
-  var sheets = {};
-  
-  sheets.authorize = authorize;
-  sheets.addBookingRow = addBookingRow;
-  
-  return sheets;
-  
-  /////////////////////////////////////////////
+(function () {
+  angular.module("app")
+    .factory("sheetsService", function (utils) {
+      var sheetsService = {};
 
-  /* Authorize the sheets API */
-  function authorize() {
-    return gapi.auth.authorize(
-      { client_id: config.apiClientId, scope: config.SCOPES, immediate: false }
-    ).then(loadSheetsApi);
-  }
+      sheetsService.authorize = authorize;
+      sheetsService.addBookingRow = addBookingRow;
 
-  /* Load the sheets client library. Returns a promise */
-  function loadSheetsApi() {
-    return gapi.client.load(config.SHEETS_API_DISCOVERY_URL);
-  }
-  
-  /* Add a booking row with the data passed */
-  function addBookingRow(data) {
-    gapi.client.sheets.spreadsheets.values.append({
-      spreadsheetId: config.bookingSheetId,
-      range: config.bookingSheet + '!A1:D1',
-      majorDimension: 'ROWS',
-      valueInputOption: 'RAW',
-      values: [convertToBookingRow(data)]
-      
-    }).then(function(response) {
-      console.log(response);
-      
-    }, function(response) {
-      utils.error(response.result.error.message);
-      
+      return sheetsService;
+
+      /////////////////////////////////////////////
+
+      /* Authorize the sheets API */
+      function authorize() {
+        return gapi.auth.authorize(
+          { client_id: config.apiClientId, scope: config.SCOPES, immediate: false }
+        ).then(loadSheetsApi);
+      }
+
+      /* Load the sheets client library. Returns a promise */
+      function loadSheetsApi() {
+        return gapi.client.load(config.SHEETS_API_DISCOVERY_URL);
+      }
+
+      /* Add a booking row with the data passed */
+      function addBookingRow(data) {
+        gapi.client.sheets.spreadsheets.values.append({
+          spreadsheetId: config.bookingSheetId,
+          range: config.bookingSheet + '!A1:D1',
+          majorDimension: 'ROWS',
+          valueInputOption: 'RAW',
+          values: [convertToBookingRow(data)]
+
+        }).then(function(response) {
+          console.log(response);
+
+        }, function(response) {
+          utils.error(response.result.error.message);
+
+        });
+      }
+
+      function convertToBookingRow(data) {
+        return [
+          data.name,
+          utils.toSheetsDate(data.date),
+          data.days,
+          data.bed
+        ];
+      }
     });
-  }
-  
-  function convertToBookingRow(data) {
-    return [
-      data.name,
-      utils.toSheetsDate(data.date),
-      data.days,
-      data.bed
-    ];
-  }
 })();
